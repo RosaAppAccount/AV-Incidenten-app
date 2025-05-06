f83e30e07cbeebb344d28c1effeae2a9776d6583
RosaAppAccount <rosa.en.co@outlook.com>
2025-05-06 13:54:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // ✅ voeg routing toe
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter> {/* ✅ maakt routes in App.jsx mogelijk */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
